ba174d213728c7ff539a2172f851575563946d2c
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-09-09 15:54:30 +0100

    Favorites Service now allows adding and removing of favorites.

---
+++ app/services/favoritesService.ts
@@ -0,0 +1,51 @@
+import {Set, util} from 'typescript-collections';
+
+export class FavoritesService {
+    private static instance: FavoritesService;
+    private static isCreating: boolean = false;
+
+    private favorites: Set<Favorite>;
+
+    constructor() {
+        if (!FavoritesService.isCreating)
+            throw new Error("FavoritesService is a singleton. Do not use new, use the getInstance method instead.");
+
+        this.favorites = new Set<Favorite>();
+    }
+
+    public static getInstance(): FavoritesService {
+        if (FavoritesService.instance == null) {
+            FavoritesService.isCreating = true;
+            FavoritesService.instance = new FavoritesService();
+            FavoritesService.isCreating = false;
+        }
+
+        return FavoritesService.instance;
+    }
+
+    public addFavorite(favorite: Favorite): boolean {
+        return this.favorites.add(favorite);
+    }
+
+    public removeFavorite(favorite: Favorite): boolean {
+        return this.favorites.remove(favorite);
+    }
+}
+
+class Favorite {
+    constructor(private studentsAssociationId: number, private modalityId: number) {
+
+    }
+
+    public getStudentsAssociationId(): number {
+        return this.studentsAssociationId;
+    }
+
+    public getModalityId(): number {
+        return this.modalityId;
+    }
+
+    public toString(): string {
+        return util.makeString(this);
+    }
+}
