e3754626ef255593ed1af21fa2114d6ad14dd285
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-09-09 14:05:59 +0100

    Created main app structure.

---
+++ app/pages/calendar/calendar.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/calendar/calendar.html'
+})
+export class CalendarPage {
+  constructor(private navCtrl: NavController) {
+
+  }
+}
